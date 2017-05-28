05d54f2ea3219f823af52b4c8740dcfc0909d649
atma0004 <atma0004@edu.eal.dk>
2017-05-28 14:31:18 +0200

    Add project files.

---
+++ Linq_And_Lamda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_And_Lamda
+{
+   
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] fibNum = { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+                double averageValue = fibNum.Where(num => num % 2 == 1).Average();
+                Console.WriteLine(averageValue);
+                Console.ReadLine();
+            }
+        }
+    
+}
+    
